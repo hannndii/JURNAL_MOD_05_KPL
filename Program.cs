1ea2b3d174b049e16fd1eabcc1c713304f099471
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-18 07:15:57 +0700

    if (nilaiTerbesar < input1)

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class pemrosesData()
+{
+    public static void dapatkanNilaiTerbesar<xT, yT, zT>(xT input1, yT input2, zT input3)
+    {
+        dynamic nilaiTerbesar = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (nilaiTerbesar < input1)
+            {
+                nilaiTerbesar = input1;
+            }
+            else if (nilaiTerbesar < input2)
+            {
+                nilaiTerbesar = input2;
+            }
+            else if (nilaiTerbesar < input3)
+            {
+                nilaiTerbesar = input3;
+            }
+        }
+
+        Console.WriteLine(nilaiTerbesar);
+    }
+
+    static void Main(string[] args)
+    {
+        long hasil;
+        dapatkanNilaiTerbesar<long, long, long>(10, 30, 22);
+
+    }
+}
